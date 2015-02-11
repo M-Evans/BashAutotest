31b8c95db0f39b43e88a8436edb10cd0621e0708
Michael Evans <mevansprogramming@gmail.com>
2015-02-11 00:07:53 -0800

    settled on final solution; removed other useless code

--- at.sh
+++ at.sh
@@ -5,45 +5,38 @@
 # change EXECUTABLE to be the relative path to your executable
 # run it, and observe!
 #
+#
 # Warning: Currently, if you want to generate a script,
 # you'll have to specify that in your input file
+# e.g. 
+#
+# script scriptName
+# dostuffhere
+# exit
+#
+# In short: wrap your code with script and exit. Also make sure
+# anything you run during your script finishes!
+
+
 
 # TODO: change to command line argument
-TEST_CASE_FILE=externFile
-# TODO: change to command line argument
-EXECUTABLE="bash -i"
+TEST_CASE_FILE=tests/exampleInput
 
-#TEMPDIR=$(mktemp -d)
 COMMAND_PID=$$
 
 
 yieldLines() {
-  #usleep 100000 # wait for the command to startup and background
-  #COMMAND_PID=$(cat $TEMPDIR/pid)
   while read p
   do # $p is the line
     usleep 100000
-    echo "$p" > /proc/$COMMAND_PID/fd/0 # goes to the screen
     echo "$p" # goes to stdin
-
   done < $TEST_CASE_FILE
 }
 
 
-echo Starting
-#sleep 1
-#echo -n " 2" # backspace characters! :)
-#sleep 1
-#echo " 1" # backspace characters! :)
-#sleep 1
-
-
 echo ================================================================================
-yieldLines | $EXECUTABLE
+yieldLines | bash -i
 echo ================================================================================
-#echo $! > $TEMPDIR/pid
-#echo waiting...
-#read p
 
 
 
