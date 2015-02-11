65be28b9ab53da54f0a6de9dd1368071aed88c15
Michael Evans <mevansprogramming@gmail.com>
2015-02-11 15:25:03 -0800

    Updated usage, implemented command line arguments, added features, and squashed some bugs

--- at.sh
+++ at.sh
@@ -1,34 +1,68 @@
 #/bin/bash
 
 # How to use:
-# change TEST_CASE_FILE to be the relative path to your inputs
-# change EXECUTABLE to be the relative path to your executable
-# run it, and observe!
+# 
+# $0 pathToTestFile
 #
-#
-# Warning: Currently, if you want to generate a script,
-# you'll have to specify that in your input file
+# Warning: Does not automatically generate a script.
+# You'll have to specify that in your input file
 # e.g. 
 #
 # script scriptName
 # dostuffhere
-# exit
 #
-# In short: wrap your code with script and exit. Also make sure
-# anything you run during your script finishes!
+# Note that you don't need to tell it to exit. When the
+# testFile is done being read, it'll send the EOF char,
+# which will stop the script automatically.
 
 
+usage() {
+  cat<<EOF
+Usage: $0 pathToTestFile
+EOF
+}
 
-# TODO: change to command line argument
-TEST_CASE_FILE=tests/exampleInput
 
-COMMAND_PID=$$
+# one argument
+if [ $# != 1 ] 
+then
+  usage
+  exit 1
+fi
+
+TEST_CASE_FILE=$1
+
+# that argument must be a file
+if [ ! -f $TEST_CASE_FILE ]
+then
+  echo "Error: $1 not a file"
+  usage
+  exit 1
+fi
+
+
+runningScripts=($(pidof script))
+runningRshells=($(pidof rshell))
+
+
+getSecond() {
+  echo $2
+}
 
 
 yieldLines() {
   while read p
   do # $p is the line
     usleep 100000
+    #bash -ic "read -p \"PAUSED\! \" var"
+    if [ ! -z "$(pidof rshell)" ]
+    then # rshell is running. Input doesn't naturally get printed
+      echo ${p} > /proc/$(pidof rshell)/fd/1
+    fi
+    if [ ! -z "$(pidof script)" ]
+    then
+      echo ${p} > /proc/$(getSecond $(pidof script))/fd/0
+    fi
     echo "$p" # goes to stdin
   done < $TEST_CASE_FILE
 }
@@ -37,6 +71,5 @@ yieldLines() {
 echo ================================================================================
 yieldLines | bash -i
 echo ================================================================================
-
 
 
